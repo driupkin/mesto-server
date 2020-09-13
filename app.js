bed2e76d2b61b81cfb463e56845652b02a7b5543
Stas Borisov <driupkin@mail.ru>
2020-09-14 02:48:27 +0300

    add Api

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
+// const cards = require('./data/cards.json');
+const cards = require('./routes/cards');
 const app = express();
 const { PORT = 3000 } = process.env;
+// app.use('/users', users);
+app.use('/cards', cards);
+// app.get('/cards', (req, res) => {
+//   res.send(cards);
+// });
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => {
-console.log('...Works');
+  console.log('...Works');
 });
