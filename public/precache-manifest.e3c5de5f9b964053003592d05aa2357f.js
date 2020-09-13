82223bd20bfc1acecfea14cff32682283a89fca8
Stas Borisov <driupkin@mail.ru>
2020-09-13 18:27:54 +0300

    add public folder

---
+++ public/precache-manifest.e3c5de5f9b964053003592d05aa2357f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9303194631430b30daed2bc36b9ffeb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa3be57d0ef159b02ee8",
+    "url": "/static/css/main.ffd4af2e.chunk.css"
+  },
+  {
+    "revision": "a549cb980bf9d2d898d1",
+    "url": "/static/js/2.096056a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.096056a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3be57d0ef159b02ee8",
+    "url": "/static/js/main.64bd3d27.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/button-add.bdfb8ebb.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/static/media/button-edit.58afbe73.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/close-icon.c2d0901f.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/edit-avatar.34c46850.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/static/media/like-active.9fdebb6b.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/static/media/logo.855a8c98.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/static/media/trash-icon.9c3b03ae.svg"
+  }
+]);
